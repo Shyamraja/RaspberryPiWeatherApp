57e39a91fc5df2f3dbc20b9f535c3d8f275abf3d
Shyamraja <t7bhsh00@students.oamk.fi>
2019-10-09 22:51:06 +0300

    weather added for sensor data

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -5,31 +5,32 @@ class Weather extends React.Component{
      render(){
        
          return(
-             <div>
+            
+            <div>
               
                
 
-                {
+                  {
                     this.props.tempr && <p className="weather__key">Temprature: 
                 <span className="weather__value">{this.props.tempr}</span></p>
-                }
+                  }
                 
                
                
-                {
+                  {
                     this.props.humidity && <p className="weather__key">Humidity:
                  <span className="weather__value">{this.props.humidity}</span></p>
-                }
+                  }
                 
                
-                {
+                  {
                     this.props. dewpoint && <p className="weather__key">Pressure:
                     <span className="weather__value">{this.props. dewpoint}</span> </p>
 
-                }
+                   }
                  
                    
-                </div>
+             </div>
         )
     }
 }
