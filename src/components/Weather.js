0afc5d2ae61c9521715f2aac954e6381f2ba26ee
Shyamraja <t7bhsh00@students.oamk.fi>
2019-10-15 07:17:29 +0300

    updated

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -5,32 +5,24 @@ class Weather extends React.Component{
      render(){
        
          return(
-            
-            <div>
-              
-               
 
-                  {
-                    this.props.tempr && <p className="weather__key">Temprature: 
-                <span className="weather__value">{this.props.tempr}</span></p>
-                  }
-                
-               
-               
-                  {
-                    this.props.humidity && <p className="weather__key">Humidity:
-                 <span className="weather__value">{this.props.humidity}</span></p>
-                  }
-                
-               
-                  {
-                    this.props. dewpoint && <p className="weather__key">Pressure:
-                    <span className="weather__value">{this.props. dewpoint}</span> </p>
+             this.props.weatherData ? this.props.weatherData.map((weather) =>
+                 <div key={weather.id}>
+                     <p className="weather__key">Date:
+                         <span className="weather__value">{weather.date_entered} </span>
+                     </p>
+                     <p className="weather__key">Temperature:
+                         <span className="weather__value">{weather.tempr} </span>
+                     </p>
+                     <p className="weather__key">Humidity:
+                         <span className="weather__value">{weather.humidity} </span>
+                     </p>
+                     <p className="weather__key">Dew point:
+                         <span className="weather__value">{weather.dewpoint} </span>
+                     </p>
+                 </div>
+             ) : null
 
-                   }
-                 
-                   
-             </div>
         )
     }
 }
